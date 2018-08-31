1f8c036de5f7228c2d2b3e11e0786c22189090f0
William Edwards <shadowapex@gmail.com>
2018-08-31 13:37:10 -0700

    Update to use steam-devices package instead of custom udev rules. Add OpenSSH package to install.

--- README.md
+++ README.md
@@ -16,8 +16,12 @@ It will do the following:
 For best results, this should be run on a fresh installation of
 Ubuntu 18.04 desktop.
 
-## Installation
+## Requirements
+In order to install SteamOS Ubuntu, you should have a fresh installation of
+Ubuntu 18.04 Desktop installed. Other versions may work, but have not been 
+tested.
 
+## Installation
 Installation is very simple. Follow these steps to install SteamOS Ubuntu:
 
 1. Clone or download this repository:    
@@ -38,3 +42,27 @@ between the two:
 * `reboot-to-steamos-mode` - sets steam as the default session and reboots
 
 You can access the terminal from Steam by adding a local shortcut for `Sakura`.
+
+## Advanced Options
+The installation script has several options that you can specify upon installation
+in the form of environment variables. You can specify these options by prefixing
+running the install script with the options you want.
+
+For example, if you want to disable installing OpenSSH and run the script non-
+interactively, you can run this command:
+
+`INCLUDE_OPENSSH=false NON_INTERACTIVE=true sudo ./install.sh`
+
+Here is the list of all the available installation options:
+
+| Option Name       | Default | Description                                       |
+| ----------------- | ------- | ------------------------------------------------- |
+| `INCLUDE_OPENSSH` | true    | Whether or not OpenSSH server should be installed |
+| `INCLUDE_SAKURA`  | true    | Whether or not to install a terminal emulator     |
+| `NON_INTERACTIVE` | false   | Whether or not to prompt the user during install  |
+| `STEAM_USER`      | steam   | The username of the account to autologin as       |
+
+## Legal
+The Steam logo and Ubuntu logo are registered trademarks of Valve Corporation
+and Canonical respectively. This project is in no way officially affiliated with
+Valve or Canonical.
--- install.sh
+++ install.sh
@@ -2,9 +2,17 @@
 
 # Set the defaults. These can be overridden by specifying the value as an
 # environment variable when running this script.
+INCLUDE_OPENSSH="${INCLUDE_OPENSSH:-true}"
+INCLUDE_SAKURA="${INCLUDE_SAKURA:-true}"
 NON_INTERACTIVE="${NON_INTERACTIVE:-false}"
 STEAM_USER="${STEAM_USER:-steam}"
 export STEAM_USER
+
+# Configure the default versions of the SteamOS packages to use. These generally
+# don't ever need to be overridden.
+STEAMOS_COMPOSITOR_VER="${STEAMOS_COMPOSITOR_VER:-1.34+bsos1_amd64}"
+STEAMOS_MODESWITCH_VER="${STEAMOS_MODESWITCH_VER:-1.10+bsos1_amd64}"
+STEAMOS_PLYMOUTH_VER="${STEAMOS_PLYMOUTH_VER:-0.17+bsos2_all}"
 
 # Ensure the script is being run as root
 if [ "$EUID" -ne 0 ]; then
@@ -34,17 +42,29 @@ STEAM_UID=$(grep "^${STEAM_USER}" /etc/passwd | cut -d':' -f3)
 STEAM_GID=$(grep "^${STEAM_USER}" /etc/passwd | cut -d':' -f4)
 echo "Steam user '${STEAM_USER}' found with UID ${STEAM_UID} and GID ${STEAM_GID}"
 
-# Install steam and a terminal emulator that can be added from Big Picture Mode
+# Install steam and steam device support.
 echo "Installing steam..."
 apt update
-apt install steam sakura -y
+apt install steam steam-devices -y
+
+# Install a terminal emulator that can be added from Big Picture Mode.
+if [[ "${INCLUDE_SAKURA}" == "true" ]]; then
+	echo "Installing the sakura terminal emulator..."
+	apt install sakura -y
+fi
+
+# Install openssh-server for remote administration
+if [[ "${INCLUDE_OPENSSH}" == "true" ]]; then
+	echo "Installing OpenSSH Server..."
+	apt install openssh-server -y
+fi
 
 # Download the packages we need. If we fail at downloading, stop the script.
 set -e
 echo "Downloading SteamOS packages..."
-wget http://repo.steamstatic.com/steamos/pool/main/s/steamos-compositor/steamos-compositor_1.34+bsos1_amd64.deb 
-wget http://repo.steamstatic.com/steamos/pool/main/s/steamos-modeswitch-inhibitor/steamos-modeswitch-inhibitor_1.10+bsos1_amd64.deb
-wget http://repo.steamstatic.com/steamos/pool/main/p/plymouth-themes-steamos/plymouth-themes-steamos_0.17+bsos2_all.deb
+wget "http://repo.steamstatic.com/steamos/pool/main/s/steamos-compositor/steamos-compositor_${STEAMOS_COMPOSITOR_VER}.deb"
+wget "http://repo.steamstatic.com/steamos/pool/main/s/steamos-modeswitch-inhibitor/steamos-modeswitch-inhibitor_${STEAMOS_MODESWITCH_VER}.deb"
+wget "http://repo.steamstatic.com/steamos/pool/main/p/plymouth-themes-steamos/plymouth-themes-steamos_${STEAMOS_PLYMOUTH_VER}.deb"
 set +e
 
 # Enable automatic login. We use 'envsubst' to replace the user with ${STEAM_USER}.
@@ -72,11 +92,6 @@ update-grub
 # Set the X session to use the installed steamos session
 echo "Configuring the default session..."
 cp ./conf/steam-session.conf "/var/lib/AccountsService/users/${STEAM_USER}"
-
-# Add support for controllers
-# source: https://steamcommunity.com/app/353370/discussions/0/490123197956024380/
-echo "Adding support for controllers inside Steam..."
-cp ./conf/99-steam-controller-perms.rules /lib/udev/rules.d/99-steam-controller-perms.rules
 
 echo ""
 echo "Installation complete! Press ENTER to reboot or CTRL+C to exit"
