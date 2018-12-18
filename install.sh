11610209b4b8b85dd8deff6e993e4064d3dbb2df
CaptSilver <davidgoree2003@gmail.com>
2018-12-18 12:00:38 -0600

    Delete the .sh during the copy process of reboot scripts.

--- install.sh
+++ install.sh
@@ -146,7 +146,8 @@ chmod +x /usr/local/sbin/reboot-to-desktop-mode
 chmod +x /usr/local/sbin/reboot-to-steamos-mode
 echo "Adding scripts to sudoers directory in case you want a password."
 echo "You will still need your user as admin to use sudo"
-cp ./conf/steamos-reboot.sh /etc/sudoers.d/
+cp ./conf/steamos-reboot.sh /etc/sudoers.d/steamos-reboot
+chmod 440 /etc/sudoers.d/steamos-reboot
 
 # Install the steamos compositor, modeswitch, and themes
 echo "Configuring the SteamOS boot themes..."
