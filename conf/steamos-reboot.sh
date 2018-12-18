661deae48e0c02c96e5e5e1bcf35c04a184ff69e
CaptSilver <davidgoree2003@gmail.com>
2018-12-17 19:16:26 -0600

    Update to reboot scripts to include root perms without password
    I forgot to change my sudoer file to run as root, oops.

--- conf/steamos-reboot.sh
+++ conf/steamos-reboot.sh
@@ -1,5 +1,5 @@
 Cmnd_Alias reboot_desktop = /usr/local/sbin/reboot-to-desktop-mode
 Cmnd_Alias reboot_steamos = /usr/local/sbin/reboot-to-steamos-mode
 
-ALL ALL = NOPASSWD:reboot_desktop
-ALL ALL = NOPASSWD:reboot_steamos
+ALL ALL = (root) NOPASSWD:reboot_desktop
+ALL ALL = (root) NOPASSWD:reboot_steamos
