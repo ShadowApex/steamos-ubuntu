583391efc9773e67575a21301f5be877af2d5ac0
CaptSilver <davidgoree2003@gmail.com>
2018-12-18 12:02:19 -0600

    Limiting the possibility of permission abuse.

--- conf/steamos-reboot.sh
+++ conf/steamos-reboot.sh
@@ -1,4 +1,5 @@
 Cmnd_Alias DESKTOP = /usr/local/sbin/reboot-to-desktop-mode
 Cmnd_Alias STEAMOS = /usr/local/sbin/reboot-to-steamos-mode
 
-ALL ALL = (root) NOPASSWD:DESKTOP,STEAMOS
+%sudo ALL = (root) NOPASSWD:DESKTOP,STEAMOS
+%admin ALL = (root) NOPASSWD:DESKTOP,STEAMOS
