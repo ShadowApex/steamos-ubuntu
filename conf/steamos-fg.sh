8422ff13b3d85572c212af18fb67bafa9e58c24f
William Edwards <shadowapex@gmail.com>
2019-01-20 16:03:55 -0800

    Match steamos-fg command line to upstream

--- conf/steamos-fg.sh
+++ conf/steamos-fg.sh
@@ -6,7 +6,7 @@ exec "$@" & # start game
 sleep 5 # wait a bit for game window
 
 # find window with size greater than 99x99 that is not a steam window
-RESULT=$(xwininfo -display ":0" -root -tree | grep -E "[0-9]{3}x[0-9]{3}" | grep -v \"Steam\": | grep -v \"SteamOverlay\":)
+RESULT=$(xwininfo -display ":0" -root -tree | grep -E "[0-9]{3}x[0-9]{3}" | grep -v \"Steam\"\: | grep -v \"SteamOverlay\"\:)
 WIN_ID=$(echo "$RESULT" | cut -d' ' -f 1)
 
 # apply STEAM_GAME property to game window so steamos compositor shows game in foreground
